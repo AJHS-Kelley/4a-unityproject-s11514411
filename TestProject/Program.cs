60a8ae4b7f6bbbf26872c9e87ad6671d4f31df0f
s11514411 <s11514411@students.duvalschools.org>
2024-02-06 14:40:19 -0800

    2/6/24_Malik_A

---
+++ TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace TestProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myInt = 7;
+        float myFloat = 7.77F;
+        bool sevenIsGreaterThanSix = true;
+        string favoriteMovie = "Fantastic Mr.Fox";
+
+        Console.WriteLine("My favorite number is " + myInt +"\n");
+        Console.WriteLine("777 divided by 100 equals " + myFloat +"\n");
+        Console.WriteLine("Seven is greater than six:" + sevenIsGreaterThanSix);
+        Console.WriteLine("My favorite movie is " +favoriteMovie);
+
+    }
+}
